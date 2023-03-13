6c45021f42dfa1e78692f30c77355aabd9fb3ce5
KellyChen520 <45199466+KellyChen520@users.noreply.github.com>
2023-03-13 17:54:35 +0800

    Update profile.js

--- indexData/profile.js
+++ indexData/profile.js
@@ -16,10 +16,3 @@ $(() => {
     });
 });
 
-// $(document).on('click', 'a[href^="#"]', function (event) {
-//     event.preventDefault();
-
-//     $('html, body').animate({
-//         scrollTop: $($.attr(this, 'href')).offset().top
-//     }, 500);
-// })
